76e44172ff1ffa5adee029814658cd0baf0ff7c6
Aadi Poddar <aadipoddarmail@gmail.com>
2022-09-01 16:44:05 +0530

    Displaying Name and Online Offline Status

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -47,4 +47,12 @@ class AuthController {
       context: context,
     );
   }
+
+  Stream<UserModel> userDataById(String userId) {
+    return authRepository.userData(userId);
+  }
+
+  void setUserState(bool isOnline) {
+    authRepository.setUserState(isOnline);
+  }
 }
